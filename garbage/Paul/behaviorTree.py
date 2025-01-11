a89bab7c8c54bc9362b5dfb7a1ad1ab70b2d3841
Paul Davesne <paul.davesne@insa-cvl.fr>
2025-01-11 18:47:35 +0100

    the behavior tree structure seems to actually work, i only need to do a bit more testing to make sure but it should be good to use

--- garbage/Paul/behaviorTree.py
+++ garbage/Paul/behaviorTree.py
@@ -3,17 +3,17 @@ class treeNode:
     def __init__(self, status):
         self.status=status
     
-    def update(self):
+    def update(self,x):
         return self.status
     
     def start(self):
-        self.update(self)
+        self.update()
 
 class ctrlNode(treeNode):
 
     def __init__(self, status, childlist):
         self.status=status
-        self.childlist=childlist                #a ctrl node has several children, thus implemented as a list
+        self.childlist=childlist                #a ctrl node has several children, here implemented as a list
 
         assert (isinstance(childlist[i],treeNode) for i in range(len(childlist)))
 
@@ -25,20 +25,16 @@ class decoNode(treeNode):
 
         assert isinstance(child,treeNode)
 
-    def update(self):
-        self.status=self.child.update(self.child)
-        return self.status
-
-
 class leafNode(treeNode):
     pass                                        #a leaf node has no children
 
 class Sequence(ctrlNode):
 
-    def update(self):
+    def update(self,x):
         for (child) in self.childlist:
             if (child.update(child)!="success"): #if the child process returns anything but success, the sequence stops updating its branches and returns failure
                 self.status="failure"
+                print(self.status)
                 return self.status
         
         self.status="success"  #else, the sequence updates all of its children and returns success 
@@ -47,8 +43,8 @@ class Sequence(ctrlNode):
     
 class Fallback(ctrlNode):
     
-    def update(self):
-        for i in (len(self.childlist)):
+    def update(self,x):
+        for i in range(len(self.childlist)):
             if (self.childlist[i].update(self.childlist[i])=="success"): #if the child process returns a success, the fallback stops updating its branches and returns success
                 self.status="success"
                 return self.status
@@ -58,32 +54,32 @@ class Fallback(ctrlNode):
 
 class UntilSuccess(decoNode):
     
-    def update(self):
-        while (self.child.status!="success"):  #I know theres a mistake here, working on it
+    def update(self,x):
+        while (self.child.status!="success"):  
             self.child.update(self.child)
         self.status="success"
         return self.status
 
 class UntilFail(decoNode):
-    def update(self):
-        while (self.child.status!="failure"):  #I know theres a mistake here, working on it
+    def update(self,x):
+        while (self.child.status!="failure"):  
             self.child.update(self.child)
         self.status="success"
         return self.status
 
 class ForceFail(decoNode):
     
-    def update(self):
-        self.child.update(self.child) #I know theres a mistake here, working on it
+    def update(self,x):
+        self.child.update(self.child) 
         self.status="failure"
         return self.status
 
 class Invert(decoNode):
     
     def update(self):
-        if (self.child.update(self.child)=="success"):  #I know theres a mistake here, working on it
+        if (self.child.update(self.child)=="success"):  
             self.status="failure"
-        elif (self.child.update(self.child)=="failure"):  #I know theres a mistake here, working on it
+        elif (self.child.update(self.child)=="failure"):  
             self.status="success"
         return self.status       
 
@@ -93,8 +89,14 @@ class Action(leafNode):
 class Condition(leafNode):
     pass
 
-class TestLeaf(Action):   
+class BehaviorTree(decoNode):
     def update(self):
+        self.status=self.child.update(self.child) 
+        return self.status
+
+
+class TestLeaf(Action):   
+    def update(self,x):
         print("test leaf")
-        self.status="success"
         return self.status 
+    
--- garbage/Paul/garbage.py
+++ garbage/Paul/garbage.py
@@ -1,16 +1,52 @@
-from behaviorTree import treeNode, ctrlNode, decoNode, leafNode, Sequence, Fallback, UntilFail, UntilSuccess, ForceFail, Action, Condition, Invert, TestLeaf
+from behaviorTree import BehaviorTree, treeNode, ctrlNode, decoNode, leafNode, Sequence, Fallback, UntilFail, UntilSuccess, ForceFail, Action, Condition, Invert, TestLeaf
 
-testTree=decoNode("waiting",
-                  Sequence("waiting",[TestLeaf,
-                                Fallback("waiting",[ForceFail("waiting",TestLeaf),
-                                          TestLeaf]),
-                                TestLeaf]))
 
-decoNode.start(testTree)
+t1=TestLeaf("success")
+t2=TestLeaf("success")
+t3=TestLeaf("failure")
+t4=TestLeaf("success")
+t5=TestLeaf("success")
+
+
+
+Ff=ForceFail("waiting",t2)
+
+F1=Fallback("waiting",[Ff,t3,t4])
+
+S1=Sequence("waiting",[t1,F1,t5])
+
+testTree=BehaviorTree("waiting",S1)
+
+
+testTree.start()
+
 
 #testing the structure of the tree
-#the test fails at Forcefail (ironically) as of now bc of the asserts
 #the resulting prints should theoretically be testing leaf repeated 4 times
 #this is NOT the final tree
 
-#removing forceFail presents other problems : update is apparently given 2 args somehow
+# Problem : the child of a given ctrl or deco node is an instance of one of the many types of nodes i have created
+# thus child has an "update(self)" method
+# how do i access the relevant update method ?
+# self.child.update(self.child) doesnt work, it seems to treat self.child as an int or sth weird like that
+
+
+#attempt at a pseudo version of what a behavior tree might look like in the final version: 
+#initialize all the nodes that dont depend on aggressivity (e.g the leaf nodes, the start nodes, the deco nodes)
+#through a if-then or case match initialize different versions of the ctrl nodes (a more agressive tree might try attacking before fleeing in the corresponding fallback node, while a more passive one might put the fleeing node first)
+
+# a snippet of the tree dealing with being attacked might look like this for exemple :
+
+# profile="aggressive"
+#
+# attack=Action()
+# flee=Action()
+# checkifenemy=Condition()
+# initialize all other nodes accordingly ...
+# ...
+# if(profile=="aggressive"):
+#     underAttack=Fallback("waiting",[checkifenemy,attack,flee])
+#     initialize the other ctrl nodes ...
+# elif(profile=="passive"):
+#     underAttack=Fallback("waiting",[checkifenemy,flee,attack])
+#     initialize the other ctrl nodes ...
