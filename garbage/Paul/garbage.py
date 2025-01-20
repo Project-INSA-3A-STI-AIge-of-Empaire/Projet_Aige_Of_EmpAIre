250146b06e5951942b116831ef05176a92eb1407
Paul Davesne <paul.davesne@insa-cvl.fr>
2025-01-20 15:03:29 +0100

    notes on implementation of nodes

--- garbage/Paul/garbage.py
+++ garbage/Paul/garbage.py
@@ -1,17 +1,17 @@
-from behaviorTree import BehaviorTree, treeNode, ctrlNode, decoNode, leafNode, Sequence, Fallback, UntilFail, UntilSuccess, ForceFail, Action, Condition, Invert, TestLeaf
+from behaviorTree import BehaviorTree, Sequence, Fallback, ForceFail, Test1, Test2, Test3, Test4, Test5,Invert
 
 
-t1=TestLeaf("success")
-t2=TestLeaf("success")
-t3=TestLeaf("failure")
-t4=TestLeaf("success")
-t5=TestLeaf("success")
+t1=Test1("success")
+t2=Test2("success")
+t3=Test3("failure")
+t4=Test4("success")
+t5=Test5("success")
 
 
 
-Ff=ForceFail("waiting",t2)
+I=Invert("waiting",t2)
 
-F1=Fallback("waiting",[Ff,t3,t4])
+F1=Fallback("waiting",[I,t3,t4])
 
 S1=Sequence("waiting",[t1,F1,t5])
 
@@ -24,12 +24,6 @@ testTree.start()
 #testing the structure of the tree
 #the resulting prints should theoretically be testing leaf repeated 4 times
 #this is NOT the final tree
-
-# Problem : the child of a given ctrl or deco node is an instance of one of the many types of nodes i have created
-# thus child has an "update(self)" method
-# how do i access the relevant update method ?
-# self.child.update(self.child) doesnt work, it seems to treat self.child as an int or sth weird like that
-
 
 #attempt at a pseudo version of what a behavior tree might look like in the final version: 
 #initialize all the nodes that dont depend on aggressivity (e.g the leaf nodes, the start nodes, the deco nodes)
